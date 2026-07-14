c85af04853b3c960fbfd72d2455baf759de03ab5
Panji Satria <ap9174016@gmail.com>
2026-07-14 10:49:05 +0700

    Update

--- server.js
+++ server.js
@@ -14,7 +14,8 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Daftarkan semua endpoint
 app.use('/api/auth', authRoutes);
